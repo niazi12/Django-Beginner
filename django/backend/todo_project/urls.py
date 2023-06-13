bc640a84a2d8179ede1d57aeb62d363994535801
Niazi Mahrab <mahrab570@gmail.com>
2023-06-13 15:24:43 +0600

    del

--- django/backend/todo_project/urls.py
+++ django/backend/todo_project/urls.py
@@ -11,6 +11,8 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
 mmmmmm
+
+gguyuigbu
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
